6c3b75c9bc5867f336dd34d2a873fe24fdf7a4ab
BJXLS <1390177059@qq.com>
2023-06-28 19:12:21 +0800

    ch03-dev ConstantInfo

---
+++ ch03/classfile/constant_pool.go
@@ -0,0 +1,85 @@
+package classfile
+
+type ConstantPool []ConstantInfo
+
+type ConstantInfo interface {
+	readInfo(reader *ClassReader) // 读取常量信息，需要具体的常量结构体实现
+}
+
+func readConstantInfo(reader *ClassReader, cp ConstantPool) ConstantInfo {
+	tag := reader.readUint8()
+	c := newConstantInfo(tag, cp)
+	c.readInfo(reader)
+	return c
+}
+
+func newConstantInfo(tag uint8, cp ConstantPool) ConstantInfo {
+	switch tag {
+	case CONSTANT_Integer:
+		return &ConstantIntegerInfo{}
+	case CONSTANT_Float:
+		return &ConstantFloatInfo{}
+	case CONSTANT_Long:
+		return &ConstantLongInfo{}
+	case CONSTANT_Double:
+		return &ConstantDoubleInfo{}
+	case CONSTANT_Utf8:
+		return &ConstantUtf8Info{}
+	case CONSTANT_String:
+		return &ConstantStringInfo{cp: cp}
+	case CONSTANT_Class:
+		return &ConstantClassInfo{cp: cp}
+	case CONSTANT_Fieldref:
+		return &ConstantFieldrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_Methodref:
+		return &ConstantMethodrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_NameAndType:
+		return &ConstantNameAnfdTypeInfo{}
+	case CONSTANT_MethodType:
+		return &ConstantMethodTypeInfo{}
+	case CONSTANT_MethodHandle:
+		return &ConstantMethodHandleInfo{}
+	case CONSTANT_InvokeDynamic:
+		return &ConstantInvokeDynamicInfo{}
+	default:
+		panic("java.lang.ClassFormatError: constant pool tag!")
+	}
+}
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantPool, cpCount)
+	for i := 1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo: // 这两种类型，占两个位置
+			i++
+		}
+	}
+	return cp
+}
+
+// 按照索引查找常量
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index!")
+}
+
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	classInfo := self.getConstantInfo(index).(*ConstantClassInfo)
+	return self.getUtf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getUtf8(index uint16) string {
+	utf8Info := self.getConstantInfo(index).(*ConstantUtf8Info)
+	return utf8Info.str
+}
